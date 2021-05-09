94abb76dab2f11d39e9884a6c836be74d3d110cf
Caroline2606 <karolina.miedzianowska@onet.pl>
2021-05-09 10:21:50 +0200

    product

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -127,5 +127,5 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 GRAPHENE = {
-    "SCHEMA": "shop.graphql.api.schema"
+    "SCHEMA": "product.graphql.api.schema"
 }
